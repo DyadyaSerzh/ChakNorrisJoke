00e55412fb48733948541f87ded37da1ae9e0e96
DyadyaSerzh <pshech1@gmail.com>
2023-02-21 07:56:09 +0200

    test redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import {Provider} from 'react-redux'
 import store from './store';
-
+import Counter from './components/counter';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+      <Counter></Counter>
         Hello
       </div>
     </Provider>
