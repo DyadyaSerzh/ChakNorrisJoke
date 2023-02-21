7198eb9edf4c355110fafd7fd101f64b85dda18a
DyadyaSerzh <pshech1@gmail.com>
2023-02-21 09:40:18 +0200

    end test redux

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,9 +1,37 @@
+import { useDispatch, useSelector } from "react-redux"
+
 
 const Counter=()=>{
+    const couster=useSelector((state) => state)
+    const dispatch=useDispatch()
 return(
         <div>
             Counter
+            {couster}
+            <button onClick={()=>dispatch({type:"INCREMENT"})}>+</button>
+            <button onClick={()=>dispatch({type:"DECREMENT"})}>-</button>
+            <button onClick={()=>dispatch({type:"RESET"})}>reset</button>
         </div>
     )
+
+
+// function todos(state = [], action) {
+//   switch (action.type) {
+//     case 'ADD_TODO':
+//       return state.concat([action.text])
+//     default:
+//       return state
+//   }
+// }
+
+// const store = createStore(todos, ['Use Redux'])
+
+// store.dispatch({
+//   type: 'ADD_TODO',
+//   text: 'Read the docs'
+// })
+
+// console.log(store.getState())
+
 }
 export default Counter
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,16 @@
 import {createStore} from 'redux';
-const initialState=[1,2];
-const reduser=(state=initialState)=>{
 
-    return state;
+const reduser=(state=0,action)=>{
+    switch (action.type){
+        case 'INCREMENT':
+            return state+1;
+        case 'DECREMENT':
+            return state-1;
+        case 'RESET':
+            return state=0;
+        default:
+            return state;
+    }
 };
 const store=createStore(reduser);
 
