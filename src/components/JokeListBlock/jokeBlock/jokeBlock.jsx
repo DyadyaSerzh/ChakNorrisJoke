21cb94b746ec003d6f722f42da2cc202b144f839
Serhii Odintsov <Serhii.Odintsov@flex.com>
2023-03-21 01:02:51 +0200

    fix css

--- src/components/JokeListBlock/jokeBlock/jokeBlock.jsx
+++ src/components/JokeListBlock/jokeBlock/jokeBlock.jsx
@@ -1,12 +1,14 @@
+import './jokeBlock.css'
 const JokeBlock=({value,id})=>{
     console.log('value----',value.value)
     let favorite=false;
     return(
-        <div id={id} className="jokeBlock">
+        <div className="jokeBlock" id={id} >
             {favorite?<span>favorite</span>:<span>NOfavorite</span>}
             <p>{value.value}</p>
             </div>
         
     )
+    // xp 86k, def 3.7k, speed 242, 
 }
 export default JokeBlock
